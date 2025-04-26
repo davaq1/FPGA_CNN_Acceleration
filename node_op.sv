6f9b49d3b0c1f19fee8195d4ad1b5096c30679ac
davaq1 <134151175+davaq1@users.noreply.github.com>
2025-04-26 18:33:32 -0400

    Update and rename node_ip.sv to node_op.sv

---
+++ node_op.sv
@@ -0,0 +1,19 @@
+module node_op(
+  input logic  clk,
+  input logic  rst,
+  input logic valid_in,
+
+  input logic prev_outputs,
+  input logic weigths,
+  input logic biases,
+  
+  output logic valid_out,
+  output logic x,
+);
+
+
+  // load all (3?) arrays
+  //do first calculation and load
+  //dp second calculation and load
+
+  
