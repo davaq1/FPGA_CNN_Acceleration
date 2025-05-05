0b57e397ac7e783109a66f2033c0c50ebd687360
davaq1 <134151175+davaq1@users.noreply.github.com>
2025-05-05 09:32:28 -0400

    Update node_op.sv

--- node_op.sv
+++ node_op.sv
@@ -1,5 +1,4 @@
 module node_op #(
-  parameter int n_inputs = 16, // for array (16 for now)
   parameter int nodes = 200,
   parameter int DW = 16 // for data width (16 for now)
   )(
@@ -7,12 +6,12 @@ module node_op #(
   input logic  rst,
   input logic valid_in,
 
-  input logic [7:0] prev_outputs [0:nodes],
-  input logic [7:0] weights    [0:nodes],
-  input logic [7:0] biases    [0:nodes],
+  input logic [15:0] prev_outputs [0:nodes-1],
+  input logic [15:0] weights    [0:nodes-1],
+  input logic [15:0] biases    [0:nodes-1],
   
   output logic valid_out,
-  output logic [15:0] x,
+  output logic [15:0] x
 );
 
 // gen. idea
@@ -21,35 +20,25 @@ module node_op #(
   //dp second calculation and load
   // place in accumulator. Then tell world x is ready (valid_out = 1)
 
-  // figure out cases below
-  //logic signed [15:0] biases      [0:M-1];
-  //logic signed [15:0] weights     [0:M*N-1];
-  //logic signed [15:0] prev_outputs[0:N-1];
-  //logic signed [31:0] x           [0:M-1];
-  integer i,j,k;
-    
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            pixel_out  <= 16'd0;
-            valid_out  <= 1'b0;
-        end else begin
-            if (valid_in) begin
-            // Update below
-             k = 0;
-              for (i = 0; i < nodes; i = i + 1) begin
-                x[i] = biases[i]; // use blocking '=' for combinational computation
-                for (j = 0; j < nodes; j = j + 1) begin
-                    x[i] = x[i] + (prev_outputs[j] * weights[k]);
-                    k = k + 1;
-                end
-                end
-                // update end
-            x <= x[0]; // review
-            valid_out <= 1'b1;
-            end else begin
-                valid_out <= 1'b0;
-            end
+integer i;
+  logic [2*DW-1:0] sum; // inter-operation
+
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      x         <= '0;
+      valid_out <= 1'b0;
+    end else begin
+      if (valid_in) begin
+        sum = '0;
+        for (i = 0; i < nodes; i++) begin
+          sum += prev_outputs[i] * weights[i] + biases[i];
         end
+        x <= sum[DW-1:0]; // 16-bits
+        valid_out <= 1'b1;
+      end else begin
+        valid_out <= 1'b0;
+      end
     end
+  end
 
 endmodule
